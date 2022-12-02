abf794b2f9a9f5bcf5a1d1915894460ec3f293a5
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-12-01 22:28:22 -0300

    feat: create splash page

---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:manhwa_track/authentication/presentation/bloc/auth/auth_bloc.dart';
+import 'package:manhwa_track/core/routes.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        state.whenOrNull(
+          authenticated: (user) =>
+              Navigator.pushReplacementNamed(context, ManhwaRoutes.second),
+          unauthenticated: () =>
+              Navigator.pushReplacementNamed(context, ManhwaRoutes.signInPage),
+        );
+      },
+      child: const Scaffold(
+        backgroundColor: Colors.black87,
+        body: Center(
+          child: CircularProgressIndicator(
+            color: Colors.white54,
+          ),
+        ),
+      ),
+    );
+  }
+}
