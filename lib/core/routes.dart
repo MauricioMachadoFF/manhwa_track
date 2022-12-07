91d45e9ce75fbc582462a6e5192b2c2416588c7e
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-12-06 21:45:51 -0300

    feat: add new route to track details page

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:manhwa_track/authentication/sign_in_up_page.dart';
-import 'package:manhwa_track/manha_tracks/home_page.dart';
+import 'package:manhwa_track/manha_tracks/presentation/bloc/validate_track_bloc.dart';
+import 'package:manhwa_track/manha_tracks/presentation/home_page.dart';
+import 'package:manhwa_track/manha_tracks/presentation/track_details_page.dart';
 import 'package:manhwa_track/splash/splash_page.dart';
 
 final Map<String, Widget Function(BuildContext)> manhwaRoutes = {
-  ManhwaRoutes.splashPage: (_) => const SplashPage(),
-  ManhwaRoutes.signInPage: (_) => const SignInUpPage(),
-  ManhwaRoutes.second: (_) => const HomePage(),
+  ManhwaRoutes.splash: (_) => const SplashPage(),
+  ManhwaRoutes.signIn: (_) => const SignInUpPage(),
+  ManhwaRoutes.homepage: (_) => const HomePage(),
+  // ManhwaRoutes.trackDetails: (context) => const TrackDetailsPage(),
+  ManhwaRoutes.trackDetails: (_) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ValidateTrackBloc>(
+          create: (_) => ValidateTrackBloc(),
+        ),
+      ],
+      child: const TrackDetailsPage(),
+    );
+  },
   //   final args = ModalRoute.of(context)?.settings.arguments;
   //   if (args == null) {
   //     const message = 'Called second route without proper params';
@@ -22,7 +36,8 @@ final Map<String, Widget Function(BuildContext)> manhwaRoutes = {
 };
 
 class ManhwaRoutes {
-  static const String splashPage = '/';
-  static const String signInPage = '/sign_in_page';
-  static const String second = '/second';
+  static const String splash = '/';
+  static const String signIn = '/sign_in_page';
+  static const String homepage = '/homepage';
+  static const String trackDetails = '/track_details';
 }
