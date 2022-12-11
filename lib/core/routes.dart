d8c37ad7c54fa7071df9240b292106d9ff074f5a
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-12-11 03:47:49 -0300

    refac: develop route manager for trackDetailsPage

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:manhwa_track/authentication/sign_in_up_page.dart';
-import 'package:manhwa_track/manha_tracks/presentation/bloc/validate_track_bloc.dart';
+import 'package:manhwa_track/core/exception.dart';
+import 'package:manhwa_track/core/injection.dart';
+import 'package:manhwa_track/core/logger.dart';
+import 'package:manhwa_track/manha_tracks/domain/entities/track.dart';
+import 'package:manhwa_track/manha_tracks/presentation/bloc/create_track/create_track_cubit.dart';
+import 'package:manhwa_track/manha_tracks/presentation/bloc/validate_track/validate_track_bloc.dart';
 import 'package:manhwa_track/manha_tracks/presentation/home_page.dart';
-import 'package:manhwa_track/manha_tracks/presentation/track_details_page.dart';
+import 'package:manhwa_track/manha_tracks/presentation/pages/track_details_page.dart';
 import 'package:manhwa_track/splash/splash_page.dart';
 
 final Map<String, Widget Function(BuildContext)> manhwaRoutes = {
   ManhwaRoutes.splash: (_) => const SplashPage(),
   ManhwaRoutes.signIn: (_) => const SignInUpPage(),
   ManhwaRoutes.homepage: (_) => const HomePage(),
-  // ManhwaRoutes.trackDetails: (context) => const TrackDetailsPage(),
-  ManhwaRoutes.trackDetails: (_) {
+  ManhwaRoutes.trackDetails: (context) {
+    final args = ModalRoute.of(context)?.settings.arguments;
+    if (args != null && args is! Track) {
+      const message = 'Called track details page with wrong params';
+      Logger().logError(
+        where: '(ManhwaRoutes): Navigation to track details page',
+        message: message,
+      );
+      throw const InvalidParamsException();
+    }
     return MultiBlocProvider(
       providers: [
         BlocProvider<ValidateTrackBloc>(
           create: (_) => ValidateTrackBloc(),
         ),
+        BlocProvider(
+          create: (_) => CreateTrackCubit(getIt.get()),
+        )
       ],
-      child: const TrackDetailsPage(),
+      child: args != null
+          ? TrackDetailsPage(track: args as Track)
+          : const TrackDetailsPage(),
     );
   },
-  //   final args = ModalRoute.of(context)?.settings.arguments;
-  //   if (args == null) {
-  //     const message = 'Called second route without proper params';
-  //     Logger().logError(where: 'ManhwaRoutes', message: message);
-
-  //     throw const MissingParamsException(message);
-  //   }
-  //   if (args is String) {
-  //     return HomePage(text: args);
-  //   }
-  //   throw const InvalidParamsException();
-  // },
 };
 
 class ManhwaRoutes {
