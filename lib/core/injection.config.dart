3541752fdcbbce44ffa37adc003cf2e5ae2297be
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-12-11 03:47:06 -0300

    feat: add more injections

--- lib/core/injection.config.dart
+++ lib/core/injection.config.dart
@@ -5,16 +5,27 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:cloud_firestore/cloud_firestore.dart' as _i4;
 import 'package:firebase_auth/firebase_auth.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:google_sign_in/google_sign_in.dart' as _i4;
+import 'package:google_sign_in/google_sign_in.dart' as _i5;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:manhwa_track/authentication/domain/auth_facade_interface.dart'
-    as _i5;
+    as _i8;
 import 'package:manhwa_track/authentication/infraestructure/firebase_auth_facade.dart'
-    as _i6;
-import 'package:manhwa_track/authentication/infraestructure/firebase_injectable_module.dart'
+    as _i9;
+import 'package:manhwa_track/manha_tracks/data/repositories/track_repository_impl.dart'
     as _i7;
+import 'package:manhwa_track/manha_tracks/domain/repositories/track_repository.dart'
+    as _i6;
+import 'package:manhwa_track/manha_tracks/domain/usecases/create_track.dart'
+    as _i10;
+import 'package:manhwa_track/manha_tracks/domain/usecases/create_user_doc.dart'
+    as _i11;
+import 'package:manhwa_track/manha_tracks/domain/usecases/get_all_tracks.dart'
+    as _i12;
+import 'package:manhwa_track/shared/module/firebase_injectable_module.dart'
+    as _i13;
 
 /// ignore_for_file: unnecessary_lambdas
 /// ignore_for_file: lines_longer_than_80_chars
@@ -32,14 +43,26 @@ extension GetItInjectableX on _i1.GetIt {
     final firebaseInjectionModule = _$FirebaseInjectionModule();
     gh.lazySingleton<_i3.FirebaseAuth>(
         () => firebaseInjectionModule.firebaseAuth);
-    gh.lazySingleton<_i4.GoogleSignIn>(
+    gh.lazySingleton<_i4.FirebaseFirestore>(
+        () => firebaseInjectionModule.firestore);
+    gh.lazySingleton<_i5.GoogleSignIn>(
         () => firebaseInjectionModule.googleSign);
-    gh.lazySingleton<_i5.AuthFacadeInterface>(() => _i6.FirebaseAuthFacade(
+    gh.factory<_i6.TrackRepository>(() => _i7.TrackRepositoryImpl(
           gh<_i3.FirebaseAuth>(),
-          gh<_i4.GoogleSignIn>(),
+          gh<_i4.FirebaseFirestore>(),
         ));
+    gh.lazySingleton<_i8.AuthFacadeInterface>(() => _i9.FirebaseAuthFacade(
+          gh<_i3.FirebaseAuth>(),
+          gh<_i5.GoogleSignIn>(),
+        ));
+    gh.factory<_i10.CreateTrack>(
+        () => _i10.CreateTrackImpl(gh<_i6.TrackRepository>()));
+    gh.factory<_i11.CreateUserDoc>(
+        () => _i11.CreateUserDocImpl(gh<_i6.TrackRepository>()));
+    gh.factory<_i12.GetAllTrack>(
+        () => _i12.GetAllTrackImpl(gh<_i6.TrackRepository>()));
     return this;
   }
 }
 
-class _$FirebaseInjectionModule extends _i7.FirebaseInjectionModule {}
+class _$FirebaseInjectionModule extends _i13.FirebaseInjectionModule {}
