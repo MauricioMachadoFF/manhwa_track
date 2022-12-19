c79a1b502b16da752874b2520128df7b4c293bfa
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-12-18 22:24:25 -0300

    refac: update home page with new ds

--- lib/manha_tracks/presentation/home_page.dart
+++ lib/manha_tracks/presentation/home_page.dart
@@ -1,14 +1,14 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:manhwa_track/authentication/presentation/bloc/auth/auth_bloc.dart';
 import 'package:manhwa_track/core/routes.dart';
+import 'package:manhwa_track/design_sytem.dart/buttons/text_button.dart';
 import 'package:manhwa_track/design_sytem.dart/design_system.dart';
 import 'package:manhwa_track/manha_tracks/presentation/bloc/load_tracks/load_track_cubit.dart';
 import 'package:manhwa_track/manha_tracks/presentation/pages/error_page.dart';
 import 'package:manhwa_track/manha_tracks/presentation/pages/loading_page.dart';
 import 'package:manhwa_track/manha_tracks/presentation/widgets/track_card.dart';
+import 'package:manhwa_track/shared/presentation/base_background_gradient.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -48,49 +48,70 @@ class _HomePageState extends State<HomePage> {
                 description:
                     "Ops! Somethign went wrong and we couldn't fetch your manhwa tracks."),
             loading: () => const LoadingPage(label: 'Fetching your tracks'),
-            loaded: (tracks) => Scaffold(
-              floatingActionButton: FloatingActionButton(
-                onPressed: () async {
-                  context.read<LoadTrackCubit>().loadTracks();
-                },
-              ),
-              body: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: spacingSmall,
-                  ),
-                  child: Stack(
-                    children: [
-                      SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            ListView.separated(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.vertical,
-                              itemBuilder: (_, index) => TrackCard(
-                                track: tracks[index],
+            loaded: (tracks) => BaseBackgroundGradient(
+              content: Scaffold(
+                backgroundColor: Colors.transparent,
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () async {
+                    // context.read<LoadTrackCubit>().loadTracks();
+                    context.read<AuthBloc>().add(const AuthEvent.signOut());
+                  },
+                ),
+                body: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: spacingSmall,
+                    ),
+                    child: Stack(
+                      children: [
+                        SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              const SizedBox(height: spacingSmall),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () => Navigator.of(context).pushNamed(
+                                    ManhwaRoutes.settings,
+                                  ),
+                                  child: Container(
+                                    padding:
+                                        const EdgeInsets.all(spacingQuarck),
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: pink,
+                                    ),
+                                    child: const Icon(
+                                      Icons.settings,
+                                      color: white,
+                                    ),
+                                  ),
+                                ),
                               ),
-                              separatorBuilder: (_, index) =>
-                                  const SizedBox(height: spacingSmall),
-                              itemCount: tracks.length,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: SizedBox(
-                          width: double.maxFinite,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.of(context)
-                                  .pushNamed(ManhwaRoutes.trackDetails);
-                            },
-                            child: Text('New Track'),
+                              const SizedBox(height: spacingSmall),
+                              ListView.separated(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.vertical,
+                                itemBuilder: (_, index) => TrackCard(
+                                  track: tracks[index],
+                                ),
+                                separatorBuilder: (_, index) =>
+                                    const SizedBox(height: spacingSmall),
+                                itemCount: tracks.length,
+                              ),
+                            ],
                           ),
                         ),
-                      )
-                    ],
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: ManhwaTextButton.onOceanBlue(
+                            title: 'New Track',
+                            onTap: () => Navigator.of(context)
+                                .pushNamed(ManhwaRoutes.trackDetails),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
